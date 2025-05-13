f70753ec24853e9270b166967e0eec8dd9742350
Nishant kumar <nishant927472@gmail.com>
2025-05-13 19:13:30 +0530

    update git

--- vite_project_8(context API)/src/context/UserContext.js
+++ vite_project_8(context API)/src/context/UserContext.js
@@ -1,5 +1,17 @@
 import React from 'react'
+import { useState } from 'react'
 
 const UserContext = React.createContext()
+
+export const UserContextProvider = ({children}) => {
+
+    const [name,setname] = useState("Nishant")
+
+    return(
+        <UserContext.Provider value={{name,setname}}>
+            {children}
+        </UserContext.Provider>
+    )
+}
 
 export default UserContext;
